bcb54cd4e00cceab3e17c2829a714416eb8487b9
Shane Carr <shane.carr@wustl.edu>
2013-05-26 03:57:57 -0500

    Implementing dynamic capabilities

--- README.md
+++ README.md
@@ -1,12 +1,77 @@
 Splittr
 =======
 
-**Splittr** is a CSS3 and JavaScript library that implements a GUI splitter.
+**Splittr** is a CSS and JavaScript library that implements a very simple GUI splitter for web applicactions.  Features include:
 
-**Splittr** does not depend on any other libraries (no jQuery)!
+* Resizable and collapsible panels
+* Lightweight: no external libraries (no jQuery or Prototype!)
+* Compatible with W3C-compliant browsers
+* Customizable look and feel
+
+Want more features?  [Fork me on Github](https://github.com/vote539/splittr)!
 
 ## Usage
 
-In your HTML, use the `data-split` attribute.
+First, include the JavaScript and CSS libraries.
+
+	<script type="text/javascript" src="splittr.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="splittr.css">
+
+In your HTML, use the `data-splittr` attribute.  It should be of the following format:
+
+	direction interactive initialBreadth1 initialBreadth2 ...
+
+where `direction` is either "horizontal" or "vertical", `interactive` is either "dynamic" (movable boundaries) or "static" (non-movable boundaries), and `initialBreadthN` is the initial width/height of the Nth panel in the splitter.
 
 **Important:** The object containing the `data-split` attribute *must* have a defined `width` and `height`, and it must be `position: relative`, `position: absolute`, or `position: fixed`.  All child elements will become `position: absolute`.
+
+### Making Panels Collapsible
+
+Panels may be made "collapsible", meaning that either of the bordering splitters has a button that enables the panel to be collapsed to zero width/height.  To enable a panel to be collapsilbe, add a `data-splittr-collapsible` attribute.
+
+## Customizing Splitters
+
+You may customize the look and size of the splitters.
+
+### Splitter Breadth
+
+In JavaScript, set the `window.splittr.barBreadth` any time before you run the `window.splittr.init()` function.  It should hold a number in pixels that represents the splitter's width/height.  Default is `5`.
+
+### Splitter Colors
+
+In CSS, override the `.splittr-bar` selector with your own colors.  For example, to make a bright blue splitter with black arrows, you could do:
+
+	.splittr-bar{
+		background: blue;
+		color: black;
+	}
+
+You may need to mark the overrides with the `!important` flag.
+
+## Example
+
+The following example makes a panel group with three panels stacked on top of each other with splitters in-between each one.  The first two panels have collapse handles.
+
+	<style type="text/css">
+	#panel1{ width: 300px; height: 250px; border: 1px solid black; position: relative; }
+	#panel2{ background-color: #666; }
+	#panel3{ background-color: #999; }
+	#panel4{ background-color: #CCC; }
+	</style>
+	<div data-splittr="horizontal dynamic 100 50" id="panel1">
+		<div data-splittr-collapsible id="panel2">
+			Alpha
+		</div>
+		<div data-splittr-collapsible id="panel3">
+			Beta
+		</div>
+		<div id="panel4">
+			Gamma
+		</div>
+	</div>
+
+## Future Improvements
+
+Features I hope to implement (or merge a pull request) include:
+
+* Expanding collapsed panels
--- splittr.js
+++ splittr.js
@@ -6,7 +6,7 @@ window.splittr.barBreadth = 5;
 window.splittr.init = function() {
 	var splitOptionsRegex = /^(horizontal|vertical) (static|dynamic) ((?:\d+ ?)+)$/;
 	window.splittr.util.get(document, function(){
-		var rawSplitOptions = this.getAttribute("data-split");
+		var rawSplitOptions = this.getAttribute("data-splittr");
 		if(!rawSplitOptions) return false;
 		var container = this;
 
@@ -16,39 +16,60 @@ window.splittr.init = function() {
 				+ "(Read '"+rawSplitOptions+"')");
 		}
 		var vertical = splitOptionsMatch[1] === "vertical";
-		var static = splitOptionsMatch[1] === "static";
+		var dynamic = splitOptionsMatch[2] === "dynamic";
 		var dimensions = splitOptionsMatch[3].split(" ");
 
+		var barContainer = document.createElement("div");
+		barContainer.setAttribute("class", "splittr-bar-container");
+
 		var pos = 0;
+		var allBar = [];
 		var children = window.splittr.util.children(container, function(idx){
 			var panel = this;
 			var dim = parseInt(dimensions[idx]) || 0;
+			this.collapsible = this.getAttribute("data-splittr-collapsible") !== null;
 
 			if(idx > 0){
-				var bar = window.splittr.util.makeBar(vertical);
-				if(vertical){
-					bar.style.left = pos+"px";
-				}else{
-					bar.style.top = pos+"px";
-				}
-				container.insertBefore(bar, panel);
+				var bar = window.splittr.util.makeBar(vertical, dynamic, pos);
+				barContainer.appendChild(bar);
+				allBar.push(bar);
 				pos += window.splittr.barBreadth;
 			}
 
 			if(vertical){
-				panel.style.width = dim+"px";
 				panel.style.left = pos+"px";
+				panel.style.width = dim+"px";
 				pos += dim;
 			}else{
-				panel.style.height = dim+"px";
 				panel.style.top = pos+"px";
+				panel.style.height = dim+"px";
 				pos += dim;
 			}
 		}, true);
+		container.appendChild(barContainer);
+		var lastChild = children[children.length-1];
 		if(vertical){
-			children[children.length-1].style.width	 = "auto";
+			lastChild.style.width = "auto";
+			lastChild.style.right = 0;
+			lastChild.style.width = lastChild.offsetWidth+"px";
 		}else{
-			children[children.length-1].style.height = "auto";
+			lastChild.style.height = "auto";
+			lastChild.style.bottom = 0;
+			lastChild.style.height = lastChild.offsetHeight+"px";
+		}
+		if(dynamic){
+			for (var i = allBar.length - 1; i >= 0; i--) {
+				var bar = allBar[i];
+				var prev = children[i];
+				var next = children[i+1];
+				window.splittr.util.handleMouseEvents(bar, prev, next, vertical);
+				if(prev.collapsible){
+					bar.addCollapse(true, vertical, prev, next);
+				}
+				if(next.collapsible){
+					bar.addCollapse(false, vertical, prev, next);
+				}
+			};
 		}
 	}, false);
 };
@@ -87,19 +108,133 @@ window.splittr.util.children = function(parent, fn, copy){
 	var elements = parent.childNodes;
 	if(copy) elements = window.splittr.util.copy(elements);
 	return window.splittr.util._elements(elements, fn);
-}
-window.splittr.util.makeBar = function(vertical, pos) {
-	var bar = document.createElement("div"), barCls;
+};
+window.splittr.util.makeBar = function(vertical, dynamic, pos) {
+	var bar = document.createElement("div"), barCls = "splittr-bar";
 	if(vertical){
-		bar.setAttribute("class", "splittr-bar splittr-bar-vertical");
+		barCls += " splittr-bar-vertical";
 		bar.style.width = window.splittr.barBreadth+"px";
+		bar.style.left = pos+"px";
 	}else{
-		bar.setAttribute("class", "splittr-bar splittr-bar-horizontal");
+		barCls += " splittr-bar-horizontal";
 		bar.style.height = window.splittr.barBreadth+"px";
+		bar.style.top = pos+"px";
+	}
+	if(dynamic){
+		barCls += " splittr-bar-dynamic";
+	}
+	bar.setAttribute("class", barCls);
+	bar.addCollapse = function(previous, vertical, prev, next){
+		var handleContainer = document.createElement("div");
+		handleContainer.setAttribute("class", "splittr-handle-container");
+		var handle = document.createElement("div");
+		handle.setAttribute("class",
+			previous ? "splittr-handle-prev" : "splittr-handle-next");
+		var handleChar = previous ?
+			(vertical ? "\u25C0" : "\u25B2"):
+			(vertical ? "\u25B6" : "\u25BC");
+		handle.appendChild(document.createTextNode(handleChar));
+		handle.addEventListener("click", function(event){
+			if(previous){
+				if(vertical){
+					bar.style.left    = (bar.offsetLeft    - prev.offsetWidth )+"px";
+					next.style.left   = (bar.offsetLeft    + bar.offsetWidth  )+"px";
+					next.style.width  = (next.offsetWidth  + prev.offsetWidth )+"px";
+					prev.style.width  = 0;
+				}else{
+					bar.style.top     = (bar.offsetTop     - prev.offsetHeight)+"px";
+					next.style.top    = (bar.offsetTop     + bar.offsetHeight )+"px";
+					next.style.height = (next.offsetHeight + prev.offsetHeight)+"px";
+					prev.style.height = 0;
+				}
+			}else{
+				if(vertical){
+					bar.style.left    = (bar.offsetLeft    + next.offsetWidth )+"px";
+					next.style.left   = (next.offsetLeft   + next.offsetWidth )+"px";
+					prev.style.width  = (prev.offsetWidth  + next.offsetWidth )+"px";
+					next.style.width  = 0;
+				}else{
+					bar.style.top     = (bar.offsetTop     + next.offsetHeight)+"px";
+					next.style.top    = (next.offsetTop    + next.offsetHeight)+"px";
+					prev.style.height = (prev.offsetHeight + next.offsetHeight)+"px";
+					next.style.height = 0;
+				}
+			}
+			window.splittr.util.dispatchSplitterMove(bar.parentNode.parentNode, prev, next);
+			// The "done" event is already fired because of the mouseup catchall
+		}, false);
+		handleContainer.appendChild(handle);
+		bar.appendChild(handleContainer);
 	}
 	return bar;
 };
-
+window.splittr.util.getTotalOffsetTop = function(element){
+	var retval = 0;
+	do {
+		retval += element.offsetTop;
+	} while (element = element.offsetParent);
+	return retval;
+};
+window.splittr.util.getTotalOffsetLeft = function(element){
+	var retval = 0;
+	do {
+		retval += element.offsetLeft;
+	} while (element = element.offsetParent);
+	return retval;
+};
+window.splittr.util.handleMouseEvents = function(bar, prev, next, vertical){
+	var origClientPos = null, origOffsetPos, minDPos, maxDPos,
+		origPrevBreadth, origNextBreadth;
+	bar.addEventListener("mousedown", function(event){
+		event.preventDefault();
+		origClientPos = vertical ? event.clientX : event.clientY;
+		origOffsetPos = vertical ? bar.offsetLeft : bar.offsetTop;
+		origPrevBreadth = vertical ? prev.offsetWidth : prev.offsetHeight;
+		origNextBreadth = vertical ? next.offsetWidth : next.offsetHeight;
+		minDPos = 0 - origPrevBreadth;
+		maxDPos = origNextBreadth;
+	}, false);
+	window.addEventListener("mousemove", function(event){
+		if(origClientPos === null) return;
+		event.preventDefault();
+		var newPos = vertical ? event.clientX : event.clientY;
+		var dPos = newPos - origClientPos;
+		if(dPos < minDPos) dPos = minDPos;
+		if(dPos > maxDPos) dPos = maxDPos;
+		if(vertical){
+			bar.style.left   = (origOffsetPos + dPos)+"px";
+			prev.style.width = (origPrevBreadth + dPos)+"px";
+			next.style.left  = (origOffsetPos + dPos + window.splittr.barBreadth)+"px";
+			next.style.width = (origNextBreadth - dPos)+"px";
+		}else{
+			bar.style.top     = (origOffsetPos + dPos)+"px";
+			prev.style.height = (origPrevBreadth + dPos)+"px";
+			next.style.top    = (origOffsetPos + dPos + window.splittr.barBreadth)+"px";
+			next.style.height = (origNextBreadth - dPos)+"px";
+		}
+		window.splittr.util.dispatchSplitterMove(next.parentNode, prev, next);
+	}, false);
+	window.addEventListener("mouseup", function(event){
+		if(origClientPos !== null){
+			origClientPos = null;
+			window.splittr.util.dispatchSplitterDone(next.parentNode, prev, next);
+		}
+	}, false);
+};
+window.splittr.util.dispatchSplitterMove = function(elem, prev, next){
+	var evnt = document.createEvent("Event");
+	evnt.initEvent("splitterMove", false, false);
+	evnt.prev = prev;
+	evnt.next = next;
+	elem.dispatchEvent(evnt);
+};
+window.splittr.util.dispatchSplitterDone = function(elem, prev, next){
+	var evnt = document.createEvent("Event");
+	evnt.initEvent("splitterDone", false, false);
+	evnt.prev = prev;
+	evnt.next = next;
+	elem.dispatchEvent(evnt);
+};
 
 document.addEventListener("DOMContentLoaded", function() {
 	splittr.init();
